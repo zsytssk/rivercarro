159c2df97729dd328157021dd7b1492289dd912e
Hugo Machet <mail@hmachet.com>
2024-05-12 17:10:04 +0200

    build: Bump version to 0.4.0

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ const Scanner = @import("zig-wayland").Scanner;
 /// tagged, the "-dev" suffix should be removed for the commit that gets tagged.
 /// Directly after the tagged commit, the version should be bumped and the "-dev"
 /// suffix added.
-const version = "0.3.0";
+const version = "0.4.0";
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
